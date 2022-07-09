f47623d87f240e3289ff34c268a59cc8aa9d1afe
cydtseng <tsengchenyu@gmail.com>
2022-07-09 20:09:11 +0800

    Added camera and facial recognition setup

---
+++ camera.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+from numpy import size
+from face_recognition import FaceRecognition
+
+#load camera
+capture = cv2.VideoCapture(0)
+#Encode faces from folder
+sfr = FaceRecognition()
+
+class Video(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        sfr.load_img_for_encoding('img/')
+    def __del__(self):
+        self.video.release()
+    def get_frame(self):
+        ret,frame=self.video.read()
+        face_locations, face_names = sfr.detect_known_faces(frame)
+        scare_img = cv2.imread('./ghost.webp')
+        scare_img_size = 700
+        scare_img = cv2.resize(scare_img, (scare_img_size, scare_img_size))
+        scare_grey =cv2.cvtColor(scare_img, cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(scare_grey, 1 ,255, cv2.THRESH_BINARY)
+
+        for face_location, name in zip(face_locations, face_names):
+            y1,x2,y2,x1 = face_location[0], face_location[1], face_location[2], face_location[3]
+            if name == 'Unknown':
+                roi = frame[-scare_img_size-10:-10, -scare_img_size-10:-10]
+                roi[np.where(mask)] = 0
+                roi += scare_img
+            else :
+                cv2.putText(frame, name, (x1, y1-10), cv2.FONT_HERSHEY_PLAIN, 1, (0,200,0),1)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0,200,0),2)
+        
+
+        ret,jpg=cv2.imencode('.jpg',frame)
+        return jpg.tobytes()
+
